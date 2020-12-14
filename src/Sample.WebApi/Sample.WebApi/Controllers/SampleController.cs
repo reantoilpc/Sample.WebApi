410145d5106b10127e2ef86d30731709f40eea8d
Ray <reantoilpc@gmail.com>
2020-12-14 23:51:29 +0800

    feat : WebApi專案建立

---
+++ src/Sample.WebApi/Sample.WebApi/Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SampleController : ControllerBase
+    {
+        public SampleController()
+        {
+        }
+
+        /// <summary>
+        /// 使用者名稱
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public string GetUserName()
+        {
+            return "rayhsu";
+        }
+    }
+}
