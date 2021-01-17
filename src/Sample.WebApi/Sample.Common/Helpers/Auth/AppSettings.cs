3fbe3033c32d5ce24c7fbba9c96ea759c8253a89
Ray <reantoilpc@gmail.com>
2021-01-17 22:29:39 +0800

    develop : 加入 AppSettings

---
+++ src/Sample.WebApi/Sample.Common/Helpers/Auth/AppSettings.cs
@@ -0,0 +1,13 @@
+namespace Sample.Common.Helpers.Auth
+{
+    /// <summary>
+    /// App Setting
+    /// </summary>
+    public class AppSettings
+    {
+        /// <summary>
+        /// Generate Token Key 
+        /// </summary>
+        public string Secret { get; set; }
+    }
+}
